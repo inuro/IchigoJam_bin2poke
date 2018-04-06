963965bf397ec4e67280f9976341c4878b63b62a
inuro <inuro@infofreako.com>
2018-04-06 13:20:04 -0700

    Update bin2poke.py

--- bin2poke.py
+++ bin2poke.py
@@ -247,7 +247,7 @@ def main():
         line_no += line_step
         if line_count == 1:
             out_file.write('%d O=#C04:FORI=0TO%d:POKE#%03x+I,(PEEK(O+I*2)-64)<<4+PEEK(O+1+I*2)-64:NEXT' % (line_no, total_bytes, poke_address_base)),
-        else
+        else:
             out_file.write('%d O=#C04:D=0:FORJ=0TO%d:N=PEEK(O-2):FORI=0TON/2-2:POKE#%03x+D,(PEEK(O+i*2)-64)<<4+PEEK(O+1+i*2)-64:D=D+1:NEXT:O=O+N+4:NEXT' % (line_no, line_count, poke_address_base)),
         out_file.write('\n')
     out_file.close()
